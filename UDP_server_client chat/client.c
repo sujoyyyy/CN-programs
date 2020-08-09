809bd0df083f78726c407e62d786fa57c8b47f7b
Sujoy Datta <sujoydatta.2009@gmail.com>
2020-08-10 01:28:02 +0530

    minor changes

---
+++ UDP_server_client chat/client.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<netdb.h>
+
+int main()
+{
+	int c_sock, n;
+	char buff[255];
+	c_sock = socket(AF_INET, SOCK_DGRAM, 0);
+	struct sockaddr_in client;
+	socklen_t *client_len;
+	client_len = sizeof(client);
+	client.sin_family = AF_INET;
+	client.sin_port = htons(9009);
+	client.sin_addr.s_addr = INADDR_ANY;
+	while(1)
+	{
+		bzero(buff, 255);
+		fgets(buff, 255, stdin);
+		n = sendto(c_sock, buff, sizeof(buff), 0, (struct sockaddr*)&client, sizeof(client));
+		bzero(buff, 255);
+		n = recvfrom(c_sock, buff, sizeof(buff), 0, (struct sockaddr*)&client, client_len);
+		printf("Server: %s\n", buff);
+		int i = strncmp("bye", buff, 3);
+		if(i==0)
+			break;
+	}
+
+	close(c_sock);
+	return 0;
+}
---
+++ UDP_server_client chat/server.c
@@ -0,0 +1,36 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+	int s_sock, n;
+	char buff[255];
+	struct sockaddr_in server, client;
+	socklen_t client_len;
+	s_sock = socket(AF_INET, SOCK_DGRAM, 0);
+	server.sin_family = AF_INET;
+	server.sin_port = htons(9009);
+	server.sin_addr.s_addr = INADDR_ANY;
+	bind(s_sock, (struct sockaddr*)&server, sizeof(server));
+	client_len = sizeof(client);
+	while(1)
+	{
+		bzero(buff, 255);
+		n = recvfrom(s_sock, buff, sizeof(buff), 0, (struct sockaddr*)&client, &client_len);
+		printf("Client: %s\n", buff);
+		bzero(buff, 255);
+		fgets(buff, 255, stdin);
+		n = sendto(s_sock, buff, sizeof(buff), 0, (struct sockaddr*)&client, sizeof(client));
+		int i = strncmp("bye", buff, 3);
+		if(i==0)
+			break;
+		
+	}
+	close(s_sock);
+	return 0;
+}
